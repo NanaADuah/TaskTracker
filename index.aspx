f3639c2e9ceeab77a1ac89af506ca812eef6a4b9
Nana Agyemang-Duah <nana321.nad@gmail.com>
2023-02-02 10:55:22 +0200

    Add project files.

---
+++ index.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="TaskTracker.index" EnableEventValidation="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Task Tracker</title>
+    <link href="styles.css" rel="stylesheet" />
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <link href="Content/fontawesome.min.css" rel="stylesheet" />
+</head>
+<script>
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
+</script>
+<body>
+    <form id="form1" runat="server">
+        <main class="container w-50">
+            <div class="d-flex align-items-center  overflow-auto">
+                <h1 class="fw-bold float-start">Task Tracker</h1>
+                <asp:Button runat="server" ID="btnClose" Text="Close" class=" text-uppercase fw-bold btn btn-danger flex-end" />
+            </div>
+            <div>
+
+                <hr />
+                <span><asp:Label runat="server" ID="lblMessages"></asp:Label></span>
+                <span>Task</span><br />
+                <asp:TextBox runat="server" class="textInput" ID="tbTaskName" />
+                <br />
+                <br />
+                <span>Day and Time</span><br />
+                <asp:TextBox runat="server" class="textInput" TextMode="DateTime" ID="tbTimeInput" />
+                <br />
+                <br />
+
+                <div class="d-flex align-items-center">
+                    <label>Set Reminder</label>
+                    <asp:CheckBox class="px-3" runat="server" ID="chkSetReminder" />
+                </div>
+                <asp:Button class="btn btn-dark align-content-center fw-bold m-auto w-100 mt-2" runat="server" Text="Save Task" ID="btnSaveTask" OnClick="btnSaveTask_Click" />
+            </div>
+            <div id="tasks">
+                <%if (values.count <= 0)
+                    { %>
+                        <div class="text-center p-3 rounded text-black" style="background-color:#eee">
+                            <asp:Label runat="server" ID="taskMessage">No tasks found, please add some first to view them.</asp:Label>
+                        </div>
+                  <%}
+                      else
+                          foreach (var task in tasks)
+                          {%>
+                            <div class="taskSingle my-2 p-0 d-flex">
+                            <%if (!task.SetReminder)
+                                {%>
+                                <div class="rounded" id="selection" style="background-color: transparent !important;"></div>
+                            <%}
+                                else
+                                { %>
+                                <div class="rounded" id="selection" style="background-color: green;"></div>
+                            <%} %>
+
+                            <div id="details" class="d-flex col bg-info px-1 rounded-1">
+                                <div class="flex-fill">
+                                   <span class="fw-bold"><%=task.TaskName%></span><br />
+                                   <span style="font-size: 0.8rem" ><%=task.TaskDate.ToString("ddd-MM-yyyy HH:mm")%></span>
+                                </div>
+                                <div class="flex-end float-end">
+                                    <asp:Button runat="server" class="px-2 text-danger border-0 fw-bold" id="iDeleteTask" Text="x" onclick="iDeleteTask_Click" />
+                                </div>
+                            </div>
+                        </div>
+                 <%} %>
+            </div>
+        </main>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace TaskTracker
+{
+    public partial class index : System.Web.UI.Page
+    {
+        public TaskCount values;
+        protected IList<Tasks> tasks;
+        private SqlConnection connection;
+        private string connStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Nana Duah\\Documents\\VS Projects\\TaskTracker\\App_Data\\Source.mdf\";Integrated Security=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            viewTasks();   
+        }
+
+        protected void btnSaveTask_Click(object sender, EventArgs e)
+        {
+            string taskName;
+            DateTime taskDate;
+            int setReminder;
+
+            taskName = tbTaskName.Text;
+            taskDate = DateTime.Now;      //TODO: Add ability to set custom date
+            setReminder = chkSetReminder.Checked ? 1 : 0;
+            connection = new SqlConnection(connStr);
+            try
+            {
+                connection.Open();
+                string command = $"INSERT INTO Tasks (TaskName, TaskDate, SetReminder) VALUES ('{taskName}','{taskDate}', '{setReminder}')";
+                SqlCommand cmd = new SqlCommand(command, connection);
+                cmd.ExecuteNonQuery();
+                lblMessages.Text = "New task added!";
+                connection.Close();
+                Response.Redirect("index.aspx");
+            }
+            catch(SqlException ex)
+            {
+                lblMessages.Text = String.Format("{0}", ex.Message);
+            }
+
+        }
+
+        private void viewTasks()
+        {
+            string command = "SELECT TaskID, TaskName, SetReminder, TaskDate FROM Tasks ORDER BY TaskDate DESC";
+            
+            tasks = new List<Tasks>();
+            values = new TaskCount();
+            connection = new SqlConnection(connStr);
+            try
+            {
+                connection.Open();
+                SqlCommand countCmd = new SqlCommand("SELECT COUNT(*) AS Total FROM [Tasks]", connection);
+                values.count = int.Parse(countCmd.ExecuteScalar().ToString());
+                countCmd.Dispose();
+
+                SqlCommand cmd = new SqlCommand(command, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (values.count > 0)
+                {
+                    while (reader.Read())
+                    {
+                        tasks.Add(new Tasks
+                        {
+                            TaskID = int.Parse(reader.GetValue(0).ToString()),
+                            TaskName = reader.GetValue(1).ToString(),
+                            SetReminder = (reader.GetValue(2).ToString() == "1") ? true : false ,
+                            TaskDate = DateTime.Parse(reader.GetValue(3).ToString()),
+                        });
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                lblMessages.Text = String.Format("{0}", ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public class TaskCount
+        {
+            public int count { get; set; }
+        }
+
+        public void deleteTask(int id)
+        {
+            
+        }
+        public class Tasks
+        {
+            public int TaskID { get; set; }
+            public string TaskName { get; set; }
+            public bool SetReminder { get; set; }
+            public DateTime TaskDate { get; set; }
+        }
+
+        protected void iDeleteTask_Click(object sender, EventArgs e)
+        {
+            values.count -= 1;
+            string query = $"DELETE FROM Tasks WHERE TaskID = {values.count}";
+            connection = new SqlConnection(connStr);
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                lblMessages.Text = "Deleted task";
+
+            }
+            catch (SqlException ex)
+            {
+                lblMessages.Text = ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
